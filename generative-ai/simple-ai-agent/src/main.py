3d3535e6b43a4febdf27e593b670b7d357b5a574
Emanuel Cortez <emanuelcortez32@gmail.com>
2026-02-27 03:04:04 -0300

    re organized folders

--- generative-ai/simple-ai-agent/src/main.py
+++ generative-ai/simple-ai-agent/src/main.py
@@ -2,17 +2,17 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_ollama import ChatOllama
 
+prompt = ChatPromptTemplate.from_messages([
+    "system", "You are a helpful assistant. Answer the user's question concisely.",
+    "human", "{question}"
+])
+
 model = "llama3.1:8b"
 llm = ChatOllama(
     model=model,
     base_url="http://localhost:11434",
     temperature=0.3
 )
-
-prompt = ChatPromptTemplate.from_messages([
-    "system", "You are a helpful assistant. Answer the user's question concisely.",
-    "human", "{question}"
-])
 
 output_parser = StrOutputParser()
 chain = prompt | llm | output_parser
