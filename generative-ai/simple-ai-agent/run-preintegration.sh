8a43954ac7c04b6c99e449d2e841cedc0b2d6ad6
Emanuel Cortez <emanuelcortez32@gmail.com>
2026-05-13 19:00:19 -0300

    refactors

--- generative-ai/simple-ai-agent/run-preintegration.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-docker compose -f docker-compose.yml kill || echo "no containers to kill"
-docker compose -f docker-compose.yml down -v || echo "no volumes to remove"
-docker compose -f docker-compose.yml rm -s -f -v || echo "no containers to remove"
-docker compose -f docker-compose.yml build
-docker compose -f docker-compose.yml up -d
